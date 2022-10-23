464ccac1a0f850abec9153ef0cfea7c607eb3567
valiv <valiv@LAPTOP-VROT9IU1>
2022-10-23 13:03:41 +0300

    Add project files.

---
+++ Data/Voin_Valentin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Voin_Valentin_Lab2.Models;
+
+namespace Voin_Valentin_Lab2.Data
+{
+    public class Voin_Valentin_Lab2Context : DbContext
+    {
+        public Voin_Valentin_Lab2Context (DbContextOptions<Voin_Valentin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Voin_Valentin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Voin_Valentin_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.Security.Policy;
+
+namespace Voin_Valentin_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+    }
+}
+
