e3b6e75cbaf4f0a88aed449423f0b335c2f626f3
valiv <valentin.voin@stud.ubbcluj.ro>
2022-11-01 20:00:18 +0200

    lab4final

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿namespace Voin_Valentin_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }   
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Voin_Valentin_Lab2.Data;
 using Voin_Valentin_Lab2.Models;
+using Voin_Valentin_Lab2.Models.ViewModels;
 
 namespace Voin_Valentin_Lab2.Pages.Categories
 {
@@ -19,13 +20,25 @@ namespace Voin_Valentin_Lab2.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IList<Category> Category { get; set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? BookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(i => i.Book)
+            .ThenInclude(i => i.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
             }
         }
     }
