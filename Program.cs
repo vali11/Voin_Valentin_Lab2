333d3843a70310826352b488f778c955fb213449
valiv <valentin.voin@stud.ubbcluj.ro>
2022-11-08 15:28:08 +0200

    lab5part2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Voin_Valentin_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Voin_Valentin_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Voin_Valentin_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Voin_Valentin_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Voin_Valentin_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Voin_Valentin_Lab2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
+
 
 var app = builder.Build();
 
@@ -22,6 +31,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
