c1151f90bc2336998e603380db2751da18674402
valiv <valentin.voin@stud.ubbcluj.ro>
2022-11-13 17:12:34 +0200

    lab5part4

--- Pages/Borrowings/Edit.cshtml.cs
+++ Pages/Borrowings/Edit.cshtml.cs
@@ -35,10 +35,19 @@ namespace Voin_Valentin_Lab2.Pages.Borrowings
             {
                 return NotFound();
             }
-            Borrowing = borrowing;
-           ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
-           ViewData["MemberID"] = new SelectList(_context.Member, "ID", "ID");
+            var bookList = _context.Book
+            .Include(b => b.Author)
+            .Select(x => new
+            {
+                x.ID,
+                BookFullName = x.Title + " - " + x.Author.LastName + " " +
+                x.Author.FirstName
+            });
+
+            ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName");
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
             return Page();
+            
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
