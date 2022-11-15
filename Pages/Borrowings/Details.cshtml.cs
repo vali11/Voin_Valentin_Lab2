99038207da522186ee76c93c865270dc33d043a2
valiv <valentin.voin@stud.ubbcluj.ro>
2022-11-15 17:35:19 +0200

    lab5final

--- Pages/Borrowings/Details.cshtml.cs
+++ Pages/Borrowings/Details.cshtml.cs
@@ -28,7 +28,11 @@ namespace Voin_Valentin_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing = await _context.Borrowing.
+                Include(b => b.Member)
+                .Include(b => b.Book)
+                .ThenInclude(b => b.Author)
+                .FirstOrDefaultAsync(m => m.ID == id);
             if (borrowing == null)
             {
                 return NotFound();
